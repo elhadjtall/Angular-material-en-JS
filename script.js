05ce3da71fe829745c19b27395d8b6ac2440006c
unknown <elhadjoumartall10@gmail.com>
2023-07-28 01:17:12 +0200

    Angular material

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-function openOnglet(evt, name){
+function openOnglet(evt, name) {
     alert('vous avez cliqué sur un bouton');
-    var i , tabcontent, tablinks;
+    var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName('tabcontent');
-    for(i= 0, i< tabcontent.length; i++){
-        tabcontent[i].computedStyleMap.display = 'None';
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = 'none';
     }
-    tablinks = document?getElementsByClassName('tablinks');
-    for (i=0; i <tablinks.length; i++){
-        tablinks[i].className = tablinks[i].className.replace("active", "");
+
+    tablinks = document.getElementsByClassName('tablinks');
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
+
     document.getElementById(name).style.display = 'block';
-    evt.currentTarget.className += "active";
+    evt.currentTarget.className += " active";
 }
